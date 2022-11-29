b41bc5113d12c545e30c254a1a72692879dc83a2
netr0m <netr0m@pm.me>
2022-11-29 19:45:56 +0100

    fix: remove obsolete url pattern (served by nginx)

--- backend/classroomconversation/classroomconversation/urls.py
+++ backend/classroomconversation/classroomconversation/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path("account/logout/", auth_views.LogoutView.as_view(), name="logout",),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
