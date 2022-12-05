45b9d02a8f5a98dda839d2026b33723345703381
netr0m <netr0m@pm.me>
2022-12-05 21:52:05 +0100

    feat: autodetect language, allow user selection feat: header for the application

---
+++ frontend/src/Design/Header.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import { useTranslation } from 'react-i18next'
+
+import { LanguagePickerDialog } from './LanguagePickerDialog'
+
+
+export const Header = () => {
+  const { t } = useTranslation()
+  const [openLangPicker, setOpenLangPicker] = useState(false)
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static' color='primary'>
+        <Toolbar>
+          <Typography
+            variant='h6'
+            noWrap
+            sx={{
+              flexGrow: 1
+            }}>
+              WeBabble
+            </Typography>
+          <Button color='inherit' onClick={() => setOpenLangPicker(true)}>{t('nav.language_picker')}</Button>
+          <Button color='inherit' onClick={() => window.open('/conversations', '_blank')}>{t('nav.admin')}</Button>
+        </Toolbar>
+      </AppBar>
+      <LanguagePickerDialog
+        open={openLangPicker}
+        onClose={() => setOpenLangPicker(false)}
+      />
+    </Box>
+  )
+}
--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en_US from './static/locales/en_US.json'
 import nb_NO from './static/locales/nb_NO.json'
@@ -11,12 +12,16 @@ const resources = {
   nn_NO,
 }
 
+export const availableLanguages = Object.keys(resources)
+
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: resources,
-    lng: "nb_NO",
-    fallbackLng: "en_US",
+    fallbackLng: availableLanguages[0] ?? 'en_US',
+    supportedLngs: availableLanguages,
+    nonExplicitSupportedLngs: true,
     debug: process.env.NODE_ENV === 'development',
     interpolation: {
       escapeValue: false
