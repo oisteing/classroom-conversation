4229d8b5448843d21172fc1eff66a5ba1797e02d
netr0m <netr0m@pm.me>
2022-12-05 20:30:21 +0100

    fix: incorrect code

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -3,19 +3,19 @@ import { initReactI18next } from 'react-i18next'
 
 import en_US from './static/locales/en_US.json'
 import nb_NO from './static/locales/nb_NO.json'
-import nb_NN from './static/locales/nn_NO.json'
+import nn_NO from './static/locales/nn_NO.json'
 
 const resources = {
   en_US,
   nb_NO,
-  nb_NN,
+  nn_NO,
 }
 
 i18n
   .use(initReactI18next)
   .init({
     resources: resources,
-    lng: "nb_NB",
+    lng: "nb_NO",
     fallbackLng: "en_US",
     debug: process.env.NODE_ENV === 'development',
     interpolation: {
